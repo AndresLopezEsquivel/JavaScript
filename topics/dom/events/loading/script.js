e2ab9bf9cfa4f3e4bb9f323aff0ab7c4556a4ffb
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-04-18 14:52:56 -0600

    Show loading events

---
+++ topics/dom/events/loading/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOMContentLoaded");
+  console.log(event);
+});
+
+window.addEventListener("load", function (event) {
+  console.log("load");
+  console.log(event);
+});
+
+window.addEventListener("beforeunload", function (event) {
+  event.preventDefault();
+  console.log(event);
+  e.returnValue = "";
+});
