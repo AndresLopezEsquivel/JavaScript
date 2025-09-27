f757c6f0d659bb38904762045f85552a5da9307a
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2025-09-27 09:44:06 -0600

    Mysterious Organism Project: Implement pAequorFactory function

---
+++ projects/mystery-organism/main.js
@@ -0,0 +1,34 @@
+// CodeCademy
+// JavaScript Syntax II
+// Challenge Project: Mysterious organism
+
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum,
+    dna,
+    mutate() {
+      const randomIndex = Math.floor(Math.random() * this.dna.length);
+      const randomlySelectedBase = this.dna[randomIndex];
+      // Removing from DNA bases the randomly selected one
+      const filteredDnaBases = ['A', 'T', 'C', 'G'].filter(base => base !== randomlySelectedBase);
+      const newBase = filteredDnaBases[Math.floor(Math.random() * filteredDnaBases.length)];
+      this.dna[randomIndex] = newBase;
+      return this.dna;
+    }
+  };
+};
